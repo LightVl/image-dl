45944495a3ed506a36dad580a911efc7e5bb1106
LightVl <mihail.rataev@gmail.com>
2024-01-05 15:12:03 +0300

    lombok fix

--- src/main/java/com/example/imagedl/Image.java
+++ src/main/java/com/example/imagedl/Image.java
@@ -1,11 +1,13 @@
 package com.example.imagedl;
 
 import lombok.Getter;
-import lombok.Setter;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class Image {
     private String message;
 }
