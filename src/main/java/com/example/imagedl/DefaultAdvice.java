7b150518b4ff013e290a58668e1e41fb97437ba6
LightVl <mihail.rataev@gmail.com>
2024-01-06 15:09:05 +0300

    Validations and custom exceptions to it

---
+++ src/main/java/com/example/imagedl/DefaultAdvice.java
@@ -0,0 +1,31 @@
+package com.example.imagedl;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class DefaultAdvice {
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Object handleRequestValidationException(Exception ex, HttpServletRequest request) {
+        Map<String, Object> responseBody = new LinkedHashMap<>();
+
+        responseBody.put("timestamp", new Date());
+        responseBody.put("status", HttpStatus.BAD_REQUEST.value());
+        responseBody.put("error", ex.getMessage());
+        responseBody.put("path", request.getServletPath());
+
+        return responseBody;
+    }
+
+}
--- src/main/java/com/example/imagedl/ImageController.java
+++ src/main/java/com/example/imagedl/ImageController.java
@@ -1,17 +1,22 @@
 package com.example.imagedl;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
+@Validated
 public class ImageController {
 
     private static final String template = "Need %s pictures about %s!";
-    //private final AtomicLong counter = new AtomicLong();
 
-    @GetMapping("/image")
-    public Image image(@RequestParam(value = "name", defaultValue = "World") String name, @RequestParam(value = "qty", defaultValue = "10") String qty) {
+    @GetMapping(value = "/image")
+    public Image getImage(@RequestParam(value = "name") @Size(min=1, max=40) String name, @RequestParam @Min(1) @Max(20) Integer qty) {
         return new Image(String.format(template, qty, name));
     }
 }
