042783e6d421ce66c2f56e876f27f19951869ece
Light <mihail.rataev@gmail.com>
2025-03-15 14:34:59 +0300

    made hibernate write in application.properties

--- src/main/java/com/example/imagedl/DBConnection.java
+++ src/main/java/com/example/imagedl/DBConnection.java
@@ -7,6 +7,10 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 @Setter
 @Getter
 @Entity
@@ -26,7 +30,20 @@ public class DBConnection {
     // Getters and setters, constructors, etc.
 
     public static void saveLog(String request, int results, String urls) {
-        SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+        //SessionFactory sessionFactory = new Configuration().configure("/application.yml").buildSessionFactory();
+        //SessionFactory sessionFactory = new Configuration().buildSessionFactory();
+            ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+            Properties properties = new Properties();
+        try {
+            properties.load(classLoader.getResourceAsStream("application.properties"));
+        } catch (IOException e) {
+            System.out.println("File not found" + e.getMessage());
+        }
+        SessionFactory sessionFactory = new Configuration()
+                .configure()
+                .addProperties(properties)
+                .addAnnotatedClass(Thread.currentThread().getContextClassLoader().getClass())
+                .buildSessionFactory();
         Session session = sessionFactory.openSession();
         DBConnection dbConnection = new DBConnection();
         dbConnection.setRequest(request);
@@ -36,6 +53,5 @@ public class DBConnection {
         session.persist(dbConnection);
         session.getTransaction().commit();
         session.close();
-        sessionFactory.close();
-    }
+        sessionFactory.close();}
 }
