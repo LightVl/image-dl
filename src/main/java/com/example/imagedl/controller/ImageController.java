18bc35f8367eee630248f7f3e3eaedace64cf2cc
Light <mihail.rataev@gmail.com>
2025-03-16 13:35:15 +0300

    added liquibase

--- src/main/java/com/example/imagedl/controller/ImageController.java
+++ src/main/java/com/example/imagedl/controller/ImageController.java
@@ -9,11 +9,8 @@ import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -33,11 +30,11 @@ public class ImageController {
     private final XmlRiverClient xmlClient;
 
     @GetMapping(value = "${jsonconfiguration.jsonpath}", produces = "application/json")
-    public String getImage(@RequestParam(value = "name") @Size(min=1, max=40) String name, @RequestParam @Min(1) @Max(20) Integer qty) throws IOException, InterruptedException {
+    public List<ImageLink> getImage(@RequestParam(value = "name") @Size(min=1, max=40) String name, @RequestParam @Min(1) @Max(20) Integer qty) throws IOException, InterruptedException {
         setQuantity(qty);
         String output = jsonSerializer(xmlClient.getImages(name));
         new Thread(() -> logService.add(new Log(name, qty, output))).start();
-        return output;
+        return xmlClient.getImages(name);
     }
     public static String jsonSerializer (List<ImageLink> images) throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
